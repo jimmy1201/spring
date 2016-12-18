2e31cdbc1dd3a4aaa6cfd522044779fcaea0f33c
jimmy <309714334@qq.com>
2016-12-18 12:28:17 +0800

    Hello World Test

---
+++ src/main/java/com/tm/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.tm;
+
+/**
+ * Created by jimmy on 2016/12/18.
+ */
+public class HelloWorld {
+
+    public static void main(String [] args){
+        System.out.print("Hello World");
+    }
+}
